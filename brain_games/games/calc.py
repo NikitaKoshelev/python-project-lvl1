97c777f0b605f4c73cfe727ba985e1305d09e50d
Nikita <Nick026rus@gmail.com>
2021-01-28 17:49:02 +0300

    Some changes

---
+++ brain_games/games/calc.py
@@ -0,0 +1,22 @@
+from random import randint, choice
+
+condition = 'What is the result of the expression?'
+
+operators = ['+', '-', '*']
+
+
+def calculating(num_1, num_2, symbol):
+    if symbol == '+':
+        expression = num_1 + num_2
+    elif symbol == '-':
+        expression = num_1 - num_2
+    elif symbol == '*':
+        expression = num_1 * num_2
+    return expression
+
+
+def get_result():
+    num1 = randint(1, 100)
+    num2 = randint(1, 10)
+    operator = choice(operators)
+    
